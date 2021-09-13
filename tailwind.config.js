59d7426768dd83688ece9f0bc272078046ded201
Syoongy <mastersyong@gmail.com>
2021-09-14 03:54:08 +0800

    custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6564db",
+        background: "#2B2B3B",
+        "nav-background": "#343444",
+        accent: "#89d2dc",
+      },
+    },
   },
   variants: {
     extend: {},
