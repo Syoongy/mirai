2b1ae544c311af741e72165c3e353cd7398b61cf
Syoongy <mastersyong@gmail.com>
2021-09-14 01:00:31 +0800

    dynamic icon item

---
+++ src/components/DynamicIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <component v-if="isLoaded" :is="heroIcons[name]" />
+</template>
+
+<script setup>
+import * as heroIcons from "@heroicons/vue/solid";
+import { onMounted, ref } from "@vue/runtime-core";
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+const isLoaded = ref(false);
+onMounted(() => {
+  isLoaded.value = true;
+});
+</script>
