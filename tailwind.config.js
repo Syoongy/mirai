8964ccd956c2b4d5255f167523028cc9d647102a
Syoongy <mastersyong@gmail.com>
2022-01-06 16:03:12 +0800

    update packages to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -11,9 +9,6 @@ module.exports = {
         accent: "#89d2dc",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
